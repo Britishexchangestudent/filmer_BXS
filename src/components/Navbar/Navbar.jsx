8af1e83fddace7d643b97fba632de853db2aa9d9
Britishexchangestudent <44755600+Britishexchangestudent@users.noreply.github.com>
2022-06-05 22:25:52 +0100

    authentication

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,20 +2,45 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable no-unused-vars */
 import { Button, IconButton, Toolbar, Avatar, Drawer, AppBar, useMediaQuery } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { useTheme } from '@mui/material/styles';
+import { useDispatch, useSelector } from 'react-redux';
 import useStyles from './styles';
 import { Sidebar, Search } from '..'
-import { fetchToken } from '../../utils';
+import { fetchToken, createSessionId, moviesApi } from '../../utils';
+import { setUser, userSelector } from '../../features/auth';
 
 function Navbar() {
+  const { isAuthenticated, user } = useSelector((state) => state.user)
   const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme()
-  const isAuthenticated = true;
+
+  const dispatch = useDispatch()
+
+  const token = localStorage.getItem('request_token');
+  const sessionIdFromLocalStorage = localStorage.getItem('session_id');
+
+  useEffect(() => {
+    const logInUser = async () => {
+      if (token) {
+        if (sessionIdFromLocalStorage) {
+          const { data: userData } = await moviesApi.get(`/account?session_id=${sessionIdFromLocalStorage}`)
+          dispatch(setUser(userData))
+        } else {
+          const sessionId = await createSessionId()
+
+          const { data: userData } = await moviesApi.get(`/account?session_id=${sessionId}`)
+
+          dispatch(setUser(userData))
+        }
+      }
+    }
+    logInUser()
+  }, [token])
     return (
       <div>
         <AppBar position="fixed">
@@ -35,7 +60,7 @@ function Navbar() {
                   Login &nbsp; <AccountCircle />
                 </Button>
                 ) : (
-                  <Button color="inherit" onClick={() => {}} component={Link} to="/profile/:id" className={classes.linkButton}>
+                  <Button color="inherit" onClick={() => {}} component={Link} to={`/profile/${user.id}`} className={classes.linkButton}>
                     {!isMobile && <>My Movies &nbsp;</>}
                     <Avatar style={{ width: 30, height: 30 }} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHUndSzxcF1UbSXX3bVILVaUbSIhoc_GEA8g&usqp=CAU" />
                   </Button>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,39 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable no-unused-vars */
+
+import { ExitToApp } from '@mui/icons-material';
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Profile() {
+  const { isAuthenticated, user } = useSelector((state) => state.user)
+
+  const favouriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  }
+
     return (
-      <div>
-        <p>Prodfile</p>
-      </div>
+      <Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h4" gutterBottom>My Profile</Typography>
+          <Button onClick={logout} color="inherit">
+            Logout &nbsp; <ExitToApp />
+          </Button>
+        </Box>
+        {!favouriteMovies.length ? (
+          <Typography variant="h5">Add favourites or watch them later</Typography>
+        ) : (
+          <Box>
+            Favourite Movies
+          </Box>
+        )}
+      </Box>
     )
 }
 
